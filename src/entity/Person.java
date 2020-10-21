28dbc7203b27458dd7c671767dda86d683cf11e7
Your Name <danja2003dd@mail.ru>
2020-10-21 12:31:45 +0300

    zagotovka

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -3,17 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sptvr19myschool;
+package entity;
 
 /**
  *
  * @author user
  */
-public class App {
-    public void run() {
-        System.out.println("----- Моя школа -----");
-        
-        
-    }
+public class Person {
     
 }
